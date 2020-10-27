78b7920e6acd4af408c296530df4476249b35858
mars8988888 <72305384+mars@users.noreply.github.com>
2020-10-27 09:48:42 +0700

    check

--- dl.js
+++ dl.js
@@ -49,33 +49,34 @@ window.onload = function(){
             buttonSecond.innerHTML = second;
         }
     }
-
-                function showTime(){
-                                    var date = new Date();
-                                    var h = date.getHours();
-                                    var m = date.getMinutes();
-                                    var s = date.getSeconds();
-                                    var session = "AM"
-
-                                    if(h == 0){
-                                        h =12;
-                                    }
-
-                                    if (h > 12){
-                                        h= h - 12;
-                                        session = "PM";
-                                    }
-
-                                    h = (h < 10) ? "0" + h : h;
-                                    m = (m < 10) ? "0" + m : m;
-                                    s = (s < 10) ? "0" + s : s;
-
-                                    var time = h + ":" + m + ":" + s + "" + session;
-                                    document.getElementById("dongho").innerText = time;
-                                    document.getElementById("dongho").textContent = time;
-                                    setTimeout(showTime, 1000);
-                                }
-                        showTime();       
-}
 //------------------------------------------make a clock-----------------------------------------------------------
-     
+    
+    function showTime(){
+                var date = new Date();
+                var h = date.getHours();
+                var m = date.getMinutes();
+          
+           var s = date.getSeconds();
+                var session = "AM"
+
+                if(h == 0){
+                h =12;
+                }
+
+                if (h > 12){
+                h= h - 12;
+                session = "PM";
+            }
+
+            h = (h < 10) ? "0" + h : h;
+            m = (m < 10) ? "0" + m : m;
+            s = (s < 10) ? "0" + s : s;
+
+            var time = h + ":" + m + ":" + s + "" + session;
+            document.getElementById("dongho").innerText = time;
+            document.getElementById("dongho").textContent = time;
+            setTimeout(showTime, 1000);                       
+                }
+    showTime();                           
+               
+}
